5d358b375566ab4287a46b82c3c0b3ea49cdb667
zzzzls <245129129@qq.com>
2024-06-20 17:57:04 +0800

    feature: Message storage, history messages

--- emoji_chat/api.py
+++ emoji_chat/api.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Body
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -6,6 +8,8 @@ from typing import List
 from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 from obscure64 import Obscure64
+
+from . import Message, MessageManager
 
 app = FastAPI()
 app.add_middleware(
@@ -16,10 +20,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")  # Serve static files
+app.mount(
+    "/static", StaticFiles(directory="static"), name="static"
+)  # Serve static files
 ob64 = Obscure64(
     b64chars="🙈🙉🙊🐒🐶🐕🐩🐺🐱😹😻😼🙀😿🐈🐯🐅🐴🐎🐮🐂🐃🐄🐷🐖🐗🐽🐑🐐🐪🐘🐭🐀🐹"
-             "🐰🐇🐻🐨🐼🐾🐔🐓🐣🐤🐥🐧🐸🐊🐢🐍🐲🐉🐳🐋🐬🐠🐡🐙🐚🐌🐛🐜🐝🐞🦋"
+    "🐰🐇🐻🐨🐼🐾🐔🐓🐣🐤🐥🐧🐸🐊🐢🐍🐲🐉🐳🐋🐬🐠🐡🐙🐚🐌🐛🐜🐝🐞🦋"
 )
 
 
@@ -34,25 +40,34 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def broadcast(self, websocket, emoji, message):
+    async def broadcast(self, websocket, message):
         for connection in self.active_connections:
             if connection != websocket:
-                await connection.send_text(f"{emoji}:{ob64.encode(message.encode('utf-8')).decode('utf-8')}")
+                await connection.send_text(message.to_json())
 
 
 manager = ConnectionManager()
+mess = MessageManager()
 
 
 @app.get("/")
 async def get(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    room_id = "0"  # 设置默认房间IDs
+    return templates.TemplateResponse(
+        request=request, name="index.html", context={"room_id": room_id}
+    )
+
+
+@app.get("/mesage/query")
+async def get_message(room_id: str):
+    return {"data": mess.query(room_id)}
 
 
 @app.post("/api/decrypt")
 async def decrypt_message(
-        body=Body(...),
+    body=Body(...),
 ):
-    return {"response": ob64.decode(body.get('message')).decode('utf-8')}
+    return {"response": ob64.decode(body.get("message")).decode("utf-8")}
 
 
 @app.websocket("/ws")
@@ -61,7 +76,15 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_text()
-            emoji, message = data.split(":", 1)
-            await manager.broadcast(websocket, emoji, message)
+            try:
+                data = json.loads(data)
+                data["msg"] = ob64.encode(data["msg"].encode("utf-8")).decode("utf-8")
+                msg = Message(**data)
+                mess.add(msg)
+                print(data, msg)
+                await manager.broadcast(websocket, msg)
+            except Exception as e:
+                raise ValueError("Message 数据错误: " + str(e))
+
     except WebSocketDisconnect:
         manager.disconnect(websocket)
