5f4605c1c0863fb41bd27b7de56916aa6f5e71c3
zzzzls <245129129@qq.com>
2024-06-28 16:49:37 +0800

    Choose to enter the room

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
-async function startNewChat() {
+async function startNewChat(room_id = null) {
     // 生成 UUID
-    const room_id = generateUUID();
+    room_id = room_id || generateUUID();
     const apiHost = window.location.host;  // 替换为你的后端API地址
 
     try {
@@ -26,8 +26,47 @@ async function startNewChat() {
 
 // 简单的UUID生成器
 function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
 }
+
+// 展示房间列表
+function show_room_list(room_list) {
+    const new_room = (data) => {
+        return `<li class="list-group-item">
+            <div class="row align-items-start">
+                <div class="col">
+                    ${data.room_id}
+                </div>
+                <div class="col">
+                    <span class="badge text-bg-primary">${data.member_count}</span>
+                </div>
+                <div class="col">
+                    <button type="button" class="btn btn-primary" onclick="startNewChat('${data.room_id}')">Join</button>
+                </div>
+            </div>
+        </li>`;
+    }
+
+    let room_dom = "";
+    for (let room of room_list) {
+        room_dom += new_room(room)
+    }
+
+    document.querySelector("#room_list > ul").innerHTML = room_dom;
+}
+
+
+(async function () {
+    // 初始化房间列表
+    try {
+        const response = await (await fetch(`/room/query`)).json();
+        // 按照房间人数降序排列
+        show_room_list(response.data.toSorted((a, b) => a.member_count - b.member_count > 0 ? -1 : 1))
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Something went wrong. Please try again.');
+    }
+})()
